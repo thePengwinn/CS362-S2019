125a5734c829bead050f4fbbeb35bb216cfa2e6c
thePengwinn <38027160+thePengwinn@users.noreply.github.com>
2019-04-21 19:32:01 -0800

    abstracted smithy

--- projects/gwinnk/dominion/dominion.c
+++ projects/gwinnk/dominion/dominion.c
@@ -686,6 +686,17 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
       }
 
+  int smithyCard(){
+    //draw 3 cards
+      for (i = 0; i < 3; i++)
+	{
+	  drawCard(currentPlayer, state);
+	}
+			
+      //discard card from hand
+      discardCard(handPos, currentPlayer, state, 0);
+    return 0;
+  }
 
 
 	
@@ -694,24 +705,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     {
     case adventurer:
     adventurerCard();
-    /*  while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }*/
+ 
       return 0;
 			
     case council_room:
@@ -857,13 +851,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 		
     case smithy:
       //+3 Cards
-      for (i = 0; i < 3; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
+      smithyCard();
+
       return 0;
 		
     case village:
